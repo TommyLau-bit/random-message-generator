40d9252883819999a61aa08ba4bd16a63fd4799f
TommyLau-bit <146777147+TommyLau-bit@users.noreply.github.com>
2025-01-04 12:15:53 +0800

    final updates

--- script.js
+++ script.js
@@ -31,5 +31,67 @@ const firstPart = [
     return `${part1} ${part2} ${part3}`;
   }
   
-  // Output Random Message
+  // Function to Display All Possible Messages
+  function showAllMessages() {
+    console.log("Here are all possible messages:");
+    firstPart.forEach((fp) => {
+      secondPart.forEach((sp) => {
+        thirdPart.forEach((tp) => {
+          console.log(`${fp} ${sp} ${tp}`);
+        });
+      });
+    });
+  }
+  
+  // Function to Add a New Message Part
+  function addMessagePart(partCategory, newMessage) {
+    switch (partCategory) {
+      case "firstPart":
+        firstPart.push(newMessage);
+        console.log(`Added to firstPart: "${newMessage}"`);
+        break;
+      case "secondPart":
+        secondPart.push(newMessage);
+        console.log(`Added to secondPart: "${newMessage}"`);
+        break;
+      case "thirdPart":
+        thirdPart.push(newMessage);
+        console.log(`Added to thirdPart: "${newMessage}"`);
+        break;
+      default:
+        console.error(`Invalid category: "${partCategory}". Choose "firstPart", "secondPart", or "thirdPart".`);
+    }
+  }
+  
+  // Function to Show All Parts in a Category
+  function showParts(partCategory) {
+    switch (partCategory) {
+      case "firstPart":
+        console.log("First Part Messages:");
+        console.log(firstPart);
+        break;
+      case "secondPart":
+        console.log("Second Part Messages:");
+        console.log(secondPart);
+        break;
+      case "thirdPart":
+        console.log("Third Part Messages:");
+        console.log(thirdPart);
+        break;
+      default:
+        console.error(`Invalid category: "${partCategory}". Choose "firstPart", "secondPart", or "thirdPart".`);
+    }
+  }
+  
+  // Display a Random Message
+  console.log("Randomized Message:");
   console.log(generateMessage());
+  
+  // Show All Possible Messages
+  console.log("\nShow All Messages (optional):");
+  showAllMessages();
+  
+  // Add a New Message to First Part and Display Updated Messages
+  console.log("\nAdding a New Message:");
+  addMessagePart("firstPart", "A wise owl hoots");
+  showParts("firstPart");
